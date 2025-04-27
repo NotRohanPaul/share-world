7e4a27735d4b4d72e4c655d4868b18e921b4d93f
Rohan Paul <75577230+NotRohanPaul@users.noreply.github.com>
2025-04-27 21:41:19 +0530

    logout basic functionality

--- backend/src/controllers/auth-controllers/logout-controller.ts
+++ backend/src/controllers/auth-controllers/logout-controller.ts
@@ -1,8 +1,8 @@
 import type { RequestHandler } from "express";
 
 export const logoutController: RequestHandler = (req, res) => {
-    // res.cookie("refreshToken", , options)
     console.log(req.cookies);
-
+    res.clearCookie("accessToken");
+    res.clearCookie("refreshToken");
     return void res.status(200).send("Logout successful");
 };
