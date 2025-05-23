e6b4fc7498282eb8570fad93716fe42aa6247c4c
Rohan Paul <75577230+NotRohanPaul@users.noreply.github.com>
2025-05-23 12:23:58 +0530

    made some scripts and config changes for consistency in frontend and backend

--- backend/rollup.config.mjs
+++ backend/rollup.config.mjs
@@ -3,12 +3,38 @@ import json from '@rollup/plugin-json';
 import resolve from '@rollup/plugin-node-resolve';
 import terser from '@rollup/plugin-terser';
 import typescript from '@rollup/plugin-typescript';
+import fs from 'node:fs';
 import { builtinModules } from 'node:module';
+import path from 'node:path';
+import gzipPlugin from 'rollup-plugin-gzip';
+import pkg from './package.json' with { type: 'json' };
+
+const { version } = pkg;
+const outDirName = 'dist';
+
+const metaGeneratorPlugin = {
+    name: 'meta-generator',
+    closeBundle() {
+        const buildNumber = Date.now();
+        const outputDir = path.join(process.cwd(), outDirName);
+
+        fs.writeFileSync(
+            path.resolve(outputDir, 'meta-backend.json'),
+            JSON.stringify({
+                version,
+                buildNumber,
+                timestamp: new Date().toISOString()
+            }, null, 2)
+        );
+        console.log('meta-backend.json created:', version);
+    }
+};
 
 export default {
     input: 'src/index.ts',
+
     output: {
-        dir: 'dist',
+        dir: outDirName
     },
     plugins: [
         json(),
@@ -22,6 +48,8 @@ export default {
                 comments: false,
             }
         }),
+        gzipPlugin(),
+        metaGeneratorPlugin
     ],
     external: (id) =>
         /^node:/.test(id) ||
