bcf21e7c9c928cae1bba10e2e8d3250eb7592921
Rohan Paul <75577230+NotRohanPaul@users.noreply.github.com>
2025-06-06 15:56:19 +0530

    moved api endpoints to the constants folder

--- frontend/src/axios/handlers/auth-handler.ts
+++ frontend/src/axios/handlers/auth-handler.ts
@@ -1,6 +1,6 @@
 import type { LoginSchemaType } from "@src/types";
 import { axiosInstance } from "../axios-instance";
-import { authEndpoints } from "../endpoints";
+import { authEndpoints } from "../../constants/api-endpoints";
 import type { AxiosResponse } from "axios";
 
 
