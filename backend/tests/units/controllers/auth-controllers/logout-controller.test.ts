b7863559bce38ce2eb92e433c72b92825d111af7
Rohan Paul <75577230+NotRohanPaul@users.noreply.github.com>
2025-07-17 15:10:29 +0530

    backend: logout - added test for logout controller

---
+++ backend/tests/units/controllers/auth-controllers/logout-controller.test.ts
@@ -0,0 +1,26 @@
+import { logoutController } from "@src/controllers/auth-controllers";
+import express from "express";
+import { describe, expect, it, vi } from "vitest";
+
+describe("test for logout controller", () => {
+    it("response 200 with clearCookie headers ", () => {
+
+        const mockResponse = {
+            send: vi.fn().mockReturnThis(),
+            status: vi.fn().mockReturnThis(),
+            clearCookie: vi.fn().mockReturnThis()
+        } as unknown as express.Response;
+
+        void logoutController({} as express.Request, mockResponse, vi.fn);
+
+        expect(mockResponse.send).toBeCalledWith("Logout successful");
+        expect(mockResponse.status).toBeCalledWith(200);
+        expect(mockResponse.clearCookie).toBeCalledTimes(2);
+        expect(mockResponse.clearCookie).toHaveBeenNthCalledWith(1, "accessToken");
+        expect(mockResponse.clearCookie).toHaveBeenNthCalledWith(2, "refreshToken");
+
+    });
+
+})
+
+
