6320fc6129e99275aabb7adc4e87aa3f5596fa39
Rohan Paul <75577230+NotRohanPaul@users.noreply.github.com>
2025-07-19 21:40:11 +0530

    frontend: ui(landing page) - added try now button in the landing page

--- frontend/src/pages/landing-page.tsx
+++ frontend/src/pages/landing-page.tsx
@@ -24,10 +24,15 @@ export const LandingPage = () => {
                         </div>
                     </nav>
                 </Navbar>
-                <main className="h-full grid grid-rows-[30%_auto_1fr] gap-5 p-2 overflow-auto [@media(max-height:40rem)]:grid-rows-none ">
+                <main className="h-full grid grid-rows-[25%_auto_auto_1fr] gap-5 p-2 overflow-auto [@media(max-height:40rem)]:grid-rows-none ">
                     <div className="flex flex-col items-center justify-center p-2">
                         <ShareWorldImgLink className="w-60 max-xs:w-[7rem] h-auto max-sm:w-40 max-sm:h-auto" />
-                        <h1 className="text-5xl max-sm:text-2xl font-semibold text-black">Share World</h1>
+                        <h1 className="text-5xl max-sm:text-2xl font-semibold">Share World</h1>
+                    </div>
+                    <div className="flex flex-col items-center">
+                        <Link className="w-[7rem] max-xs:w-[6rem] text-xl max-xs:text-base text-center font-bold p-2  max-xs:py-1.5 rounded-full outline-3 outline-primary transition-shadow shadow-[5px_-5px_0_5px_var(--app-color-primary)] hover:shadow-none" to={"/user"}>
+                            Try Now
+                        </Link>
                     </div>
                     <div className="flex flex-col items-center">
                         <motion.h1
