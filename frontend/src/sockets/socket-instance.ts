1f10a3d89dc9fe1c194d84097af2a0b972b4a182
Rohan Paul <75577230+NotRohanPaul@users.noreply.github.com>
2025-07-27 14:25:54 +0530

    frontend: socket timeouts - changed to exponent notation for readability

--- frontend/src/sockets/socket-instance.ts
+++ frontend/src/sockets/socket-instance.ts
@@ -5,8 +5,8 @@ export const sharedSocketOptions = {
     path: "/socket/v1",
     autoConnect: false,
     reconnectionAttempts: 5,
-    reconnectionDelay: 1000,
-    reconnectionDelayMax: 5000,
+    reconnectionDelay: 1e3,
+    reconnectionDelayMax: 5e3,
 };
 
 export const shareViaIdSocketInstance = io(API_ORIGIN + "/share-via-id", sharedSocketOptions);
