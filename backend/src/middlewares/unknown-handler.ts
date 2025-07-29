5c796ceb5b1d0dbefca22f564f12f008b73537b8
Rohan Paul <75577230+NotRohanPaul@users.noreply.github.com>
2025-07-29 12:16:48 +0530

    backend: middleware - change the reponse for unknown routes to not found

--- backend/src/middlewares/unknown-handler.ts
+++ backend/src/middlewares/unknown-handler.ts
@@ -2,5 +2,5 @@ import type { RequestHandler } from "express";
 import { HTTP_STATUS_CODES } from "@src/constants/http-status-codes";
 
 export const unknownHandler: RequestHandler = (_req, res) => {
-    res.sendStatus(HTTP_STATUS_CODES.BAD_REQUEST);
+    res.sendStatus(HTTP_STATUS_CODES.NOT_FOUND);
 };
