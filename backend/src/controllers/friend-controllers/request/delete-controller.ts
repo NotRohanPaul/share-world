64efafb3b2602ff008f2c5d65b03375a1fa29eac
Rohan Paul <75577230+NotRohanPaul@users.noreply.github.com>
2025-08-01 16:09:07 +0530

    backend: delete request - added delete request controller and basic test with rest client

--- backend/src/controllers/friend-controllers/request/delete-controller.ts
+++ backend/src/controllers/friend-controllers/request/delete-controller.ts
@@ -1,6 +1,48 @@
-import type { AuthContextHandlerType } from "@src/types/context";
+import { appLogger } from "@src/configs/app-logger";
+import { HTTP_STATUS_CODES } from "@src/constants/http-status-codes";
+import type { SenderReceiverContextHandlerType } from "@src/middlewares/friends/sender-receiver-checks-middleware";
+import { FriendRequestModel } from "@src/models/friend-request.model";
+import { UserModel } from "@src/models/users.model";
+import { ZodError } from "zod";
 
 
-export const deleteController: AuthContextHandlerType = () => {
+export const deleteController: SenderReceiverContextHandlerType = async (_req, res) => {
+    try {
+        const senderEmail = res.locals.context?.auth?.email;
+        const receiverEmail = res.locals.context?.receiverEmail;
+        const senderId = res.locals.context?.senderId;
+        const receiverId = res.locals.context?.receiverId;
 
+        const isRequestAlreadyExists = await FriendRequestModel.exists({
+            sender: senderId,
+            receiver: receiverId,
+        });
+
+        if (isRequestAlreadyExists === null) {
+            return void res.status(HTTP_STATUS_CODES.BAD_REQUEST).send("No Request to delete");
+        }
+
+        const isAlreadyFriends = await UserModel.exists({
+            email: senderEmail,
+            friendsEmailList: receiverEmail
+        });
+
+        if (isAlreadyFriends !== null) {
+            return void res.status(HTTP_STATUS_CODES.BAD_REQUEST).send("Already both are friends");
+        }
+
+        await FriendRequestModel.deleteOne({
+            sender: senderId,
+            receiver: receiverId,
+        });
+
+        return void res.sendStatus(HTTP_STATUS_CODES.OK);
+    }
+    catch (e) {
+        appLogger.error({ e });
+        if (e instanceof ZodError) {
+            return void res.sendStatus(HTTP_STATUS_CODES.BAD_REQUEST);
+        }
+        return void res.sendStatus(HTTP_STATUS_CODES.INTERNAL_SERVER_ERROR);
+    }
 };
