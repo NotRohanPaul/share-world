bbc037803894c34e874851b443ab444f84825d3b
Rohan Paul <75577230+NotRohanPaul@users.noreply.github.com>
2025-08-05 10:45:47 +0530

    frontend: ui - removed error parargraph and added error toast in th receiver side

--- frontend/src/components/share-via-id/receiver/layout/receiver-main.tsx
+++ frontend/src/components/share-via-id/receiver/layout/receiver-main.tsx
@@ -4,6 +4,8 @@ import { UserId } from "../../common/user-id";
 import { SenderId } from "../ui/sender-id";
 import { FileList } from "../../common/files-list";
 import { UserHeader, UserMain, UserSection } from "../../common/layout/user-layouts";
+import { useEffect } from "react";
+import { useToastConsumer } from "@src/components/common/ui/toast/context/toasts-consumer";
 
 export const ReceiverViaIdMain = () => {
     const {
@@ -13,12 +15,23 @@ export const ReceiverViaIdMain = () => {
         fileList
     } = useReceiver();
 
+    const showToast = useToastConsumer();
+
+    useEffect(() => {
+        if (error === null || error.trim() === "") return;
+
+        showToast({
+            text: error,
+            exitDelay: 1e3
+        });
+
+    }, [error]);
+
     return (
         <UserSection>
             <UserHeader>
                 <AppIcons.Receive className="w-[3rem] h-[3rem] max-xs:w-[2rem] max-xs:h-[2rem] flex items-center relative -bottom-1 max-xs:-bottom-1" />
                 <h1 className="text-5xl max-xs:text-4xl font-semibold">Receiver</h1>
-                {error && <p className="text-orange-500">Error: {error}</p>}
             </UserHeader>
             <UserMain>
                 <UserId userId={userId} peerType="receiver" />
