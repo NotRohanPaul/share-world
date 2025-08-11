f12fa6c4ef0aee01e36a76bb5d25bae6269b3309
Rohan Paul <75577230+NotRohanPaul@users.noreply.github.com>
2025-08-11 15:18:14 +0530

    frontend: axios - added endpoints for requests handlers

--- frontend/src/axios/endpoints/api-endpoints.ts
+++ frontend/src/axios/endpoints/api-endpoints.ts
@@ -7,6 +7,14 @@ export const authEndpoints = {
 export const friendListEndpoints = {
     sentRequests: "/friend/list/sent-requests",
     receivedRequests: "/friend/list/received-requests",
-    blocks: "/friend/list",
-    friends: "/friend/list",
+    blocks: "/friend/list/blocks",
+    friends: "/friend/list/friends",
+};
+
+export const friendRequestEndpoints = {
+    send: "/friend/request/send",
+    delete: "/friend/request/delete",
+    reject: "/friend/request/reject",
+    accept: "/friend/request/accept",
+    block: "/friend/request/block",
 };
