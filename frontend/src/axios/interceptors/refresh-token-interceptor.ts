bfcdcd3e7dd02ba58b847520a1820e0d0ec5cdcd
Rohan Paul <75577230+NotRohanPaul@users.noreply.github.com>
2025-08-11 15:19:14 +0530

    frontend: axios - added a basic refresh interceptor for unauthorized request

--- frontend/src/axios/interceptors/refresh-token-interceptor.ts
+++ frontend/src/axios/interceptors/refresh-token-interceptor.ts
@@ -9,19 +9,19 @@ export const createRefreshTokenInterceptor = (
 > => {
     return {
         onFulfilled: async (response: AxiosResponse): Promise<AxiosResponse> => {
-            type ResponseWithRetryType = AxiosResponse & { _retry: boolean; };
-            if (response.status === HTTP_STATUS_CODES.UNAUTHORIZED && (response as ResponseWithRetryType)._retry === false) {
+            if (response.config.url?.includes("/auth/refresh-token")) {
+                console.log("same refresh");
+                return response;
+            }
+
+            if (response.status === HTTP_STATUS_CODES.UNAUTHORIZED) {
                 try {
-                    (response as ResponseWithRetryType)._retry = true;
-
+                    console.log("Refresh Endpoint");
                     await axiosInstance.get("/auth/refresh-token");
-
-                    return axiosInstance(response.config);
                 } catch {
                     return response;
                 }
             }
-
             return response;
         },
     };
