930c408738dd22659fa49ef38b0f50f603c0bd40
Rohan Paul <75577230+NotRohanPaul@users.noreply.github.com>
2025-08-13 14:20:28 +0530

    backend: routes - changed delete request method from post to delete

--- backend/src/routes/friend-router.ts
+++ backend/src/routes/friend-router.ts
@@ -17,7 +17,7 @@ import { Router } from "express";
 
 const requestRouter = Router();
 requestRouter.post("/send", sendController);
-requestRouter.post("/delete", deleteController);
+requestRouter.delete("/delete", deleteController);
 requestRouter.post("/reject", rejectController);
 requestRouter.post("/accept", acceptController);
 requestRouter.post("/block", blockController);
