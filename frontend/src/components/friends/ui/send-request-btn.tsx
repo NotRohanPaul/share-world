23512300f66648a4e90c174bb567d5ceb7587207
Rohan Paul <75577230+NotRohanPaul@users.noreply.github.com>
2025-08-13 21:59:42 +0530

    frontend: request-btn - added a basic implementation of request btn

--- frontend/src/components/friends/ui/send-request-btn.tsx
+++ frontend/src/components/friends/ui/send-request-btn.tsx
@@ -1,11 +1,136 @@
+import { apiHandlers } from "@src/axios/handlers/api-handlers";
+import { useDebounce } from "@src/components/common/hooks/useDebounce";
+import { useDialogBoxConsumer } from "@src/components/common/ui/dialog-box/context/dialog-box-consumer";
+import { DialogButton } from "@src/components/common/ui/dialog-box/ui/dialog-btn";
+import { loginSchema } from "@src/schemas/auth-schemas";
+import { useMutation } from "@tanstack/react-query";
+import { AnimatePresence, motion } from "motion/react";
+import {
+    useState,
+    type ChangeEvent,
+    type KeyboardEvent,
+} from "react";
 
+const SendRequestDialog = () => {
+    const [receiverEmail, setReceiverEmail] = useState<string>('');
+    const [emailError, setEmailError] = useState<string>('');
+    const [serverResponse, setServerResponse] = useState<string>('');
+
+    useDebounce(() => {
+        if (receiverEmail === "") {
+            return void setEmailError('');
+        }
+        const result = loginSchema.shape.email.safeParse(receiverEmail);
+        setEmailError(result.success === true ? '' : result.error.issues[0].message);
+    }, [receiverEmail], 600);
+
+    const { mutate: sendRequest } = useMutation({
+        mutationFn: apiHandlers.friendRequest.send,
+        onSuccess: (res) => {
+            if (res.status === 200) {
+                setServerResponse("Request Sent Sucessfully");
+            }
+            else {
+                setServerResponse("Something went wrong");
+            };
+        },
+        onError: () => {
+            setServerResponse("Network error");
+        }
+    });
+
+    const handleInput = (e: ChangeEvent<HTMLInputElement>) => {
+        setServerResponse('');
+        setReceiverEmail(e.target.value);
+    };
+
+    const handleSendClick = () => {
+        setServerResponse('');
+        sendRequest({ receiverEmail });
+    };
+
+    const handleEnter = (e: KeyboardEvent<HTMLInputElement>) => {
+        if (e.key === "Enter") {
+            setServerResponse('');
+            sendRequest({ receiverEmail });
+        }
+    };
+
+    return (
+        <section className="h-full flex flex-col justify-between gap-5">
+            <div className="flex flex-col gap-2">
+                <AnimatePresence mode="wait">
+                    {serverResponse !== '' && (
+                        <motion.p
+                            key="error"
+                            initial={{ opacity: 0, height: 0 }}
+                            animate={{ opacity: 1, height: "auto" }}
+                            exit={{ opacity: 0, height: 0 }}
+                            transition={{ duration: 0.15 }}
+                            className="text-base text-orange-700"
+                        >
+                            {serverResponse}
+                        </motion.p>
+                    )}
+                </AnimatePresence>
+                <label
+                    htmlFor="receiver-email"
+                    className="font-semibold"
+                >
+                    Enter receiver email
+                </label>
+                <input
+                    autoFocus={true}
+                    id="receiver-email"
+                    type="email"
+                    placeholder="hello@example.com"
+                    value={receiverEmail}
+                    onChange={handleInput}
+                    onKeyDown={handleEnter}
+                    className="outline-1 px-2 py-2 outline-gray-500 rounded-sm focus:outline-2 focus:outline-primary placeholder:text-gray-600"
+                />
+                <AnimatePresence mode="wait">
+                    {emailError !== '' && (
+                        <motion.p
+                            key="error"
+                            initial={{ opacity: 0, height: 0 }}
+                            animate={{ opacity: 1, height: "auto" }}
+                            exit={{ opacity: 0, height: 0 }}
+                            transition={{ duration: 0.15 }}
+                            className="text-base text-orange-700"
+                        >
+                            {emailError}
+                        </motion.p>
+                    )}
+                </AnimatePresence>
+            </div>
+            <div className="self-end justify-self-end">
+                <DialogButton
+                    onClick={handleSendClick}
+                >
+                    Send
+                </DialogButton>
+            </div>
+        </section>
+    );
+};
 
 export const SendRequestBtn = () => {
+    const { showDialogBox } = useDialogBoxConsumer();
+
+    const handleSendRequest = () => {
+        showDialogBox({
+            type: "component",
+            children: <SendRequestDialog />,
+        });
+    };
+
     return (
         <button
             className="primary-btn p-2 rounded-sm"
+            onClick={handleSendRequest}
         >
             Send Request
-        </button>
+        </button >
     );
 };
