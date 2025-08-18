cab67191d2553fcb0a84749d71c95e441745beb3
Rohan Paul <75577230+NotRohanPaul@users.noreply.github.com>
2025-08-18 13:59:41 +0530

    frontend: error page - made the text and button liquid to smaller scrrens

--- frontend/src/pages/error-page.tsx
+++ frontend/src/pages/error-page.tsx
@@ -10,24 +10,23 @@ export const ErrorPage = () => {
                 <title>Error</title>
             </Helmet>
             <main className="h-[100dvh] grid place-content-center gap-3">
-                <h1 className="text-5xl max-sm:text-2xl font-bold">
+                <h1 className="text-5xl max-sm:text-3xl max-xs:text-base font-bold">
                     Something went wrong!
                 </h1>
-                <div className="justify-self-center flex gap-2">
+                <div className="justify-self-center flex flex-0 gap-2 text-xl max-xs:text-sm">
                     <Link
                         to={appRoutes.home.absolute}
-                        className="primary-btn w-[100px] max-sm:w-[70px] flex items-center justify-center text-xl px-4 py-3 max-sm:text-base max-sm:p-2"
+                        className="primary-btn min-w-[100px] max-xs:min-w-[30px] max-xs:h-[30px] flex items-center justify-center p-3 max-xs:px-5"
                     >
                         Home
                     </Link>
                     <button
                         onClick={() => window.location.reload()}
-                        className="primary-btn w-[100px] max-sm:w-[70px] flex items-center justify-center text-xl px-4 py-3 max-sm:text-base max-sm:p-2"
+                        className="primary-btn min-w-[100px] max-xs:min-w-[30px] max-xs:h-[30px] flex items-center justify-center p-3 max-xs:px-5"
                     >
                         Reload
                     </button>
                 </div>
-
             </main>
         </>
     );
