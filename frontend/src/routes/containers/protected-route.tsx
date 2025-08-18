b52f045d354afdcfeda9e07eece0b917a35d5ade
Rohan Paul <75577230+NotRohanPaul@users.noreply.github.com>
2025-08-18 14:56:07 +0530

    frontend: protected route - replace children with outlet as children doesnt work with react router

--- frontend/src/routes/containers/protected-route.tsx
+++ frontend/src/routes/containers/protected-route.tsx
@@ -1,19 +1,13 @@
 import { UnauthorizedPage } from "@src/pages/unauthorized-page";
 import { authSelectors } from "@src/redux/slices/auth";
 import { useAppSelector } from "@src/redux/utils/hooks";
-import { type PropsWithChildren } from "react";
+import { Outlet } from "react-router";
 
-export const ProtectedRoute = ({ children }: PropsWithChildren) => {
+export const ProtectedRoute = () => {
     const userState = useAppSelector(authSelectors.user);
-    if (userState.type !== "auth-user") {
-        return (
-            <UnauthorizedPage />
-        );
-    }
 
-    return (
-        <>
-            {children}
-        </>
-    );
+    if (userState.type !== "auth-user")
+        return <UnauthorizedPage />;
+
+    return <Outlet />;
 };
