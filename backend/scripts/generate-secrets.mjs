39a613656c7377b8c46aee208298063bc4acccd9
Rohan Paul <75577230+NotRohanPaul@users.noreply.github.com>
2025-08-21 19:55:27 +0530

    backend: scripts - made the secret generator take inputs

--- backend/scripts/generate-secrets.mjs
+++ backend/scripts/generate-secrets.mjs
@@ -1,2 +1,29 @@
+import readline from "node:readline";
 import crypto from "node:crypto";
-console.log("Secret:", crypto.randomBytes(64).toString("hex"));
+
+
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+});
+
+rl.question("Enter a number: ", (input) => {
+    console.log("Input Received", input);
+
+    const number = Number(input);
+
+    if (
+        Number.isSafeInteger(number) === false ||
+        number <= 0 ||
+        input.includes('.') ||
+        input.toLowerCase().includes('e')
+    ) {
+        console.log("Invalid input! Enter a positive whole number only.");
+        rl.close();
+        return;
+    }
+
+    console.log("Secret:", crypto.randomBytes(number).toString("hex"));
+    rl.close();
+})
+
