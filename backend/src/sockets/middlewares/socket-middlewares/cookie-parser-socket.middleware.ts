86a2fcda42221c807d3f2f3c66326a0be64483b0
Rohan Paul <75577230+NotRohanPaul@users.noreply.github.com>
2025-08-23 14:51:44 +0530

    backend: socket-middleware - added cookie parser to parse the same way as did in express

---
+++ backend/src/sockets/middlewares/socket-middlewares/cookie-parser-socket.middleware.ts
@@ -0,0 +1,40 @@
+import { cookiePraserWithEncryptionMiddleware } from "@src/middlewares/common/cookie-parser-with-encryption.middleware";
+import express from "express";
+import type { SocketMiddlewareType } from "../../types";
+import { appLogger } from "@src/configs/app-logger";
+
+
+export const socketCookieMiddleware: SocketMiddlewareType<{
+    socketData: {
+        cookies: Record<string, unknown>,
+        signedCookies?: Record<string, unknown>,
+    };
+}> = (socket, next) => {
+    try {
+        const req = { headers: socket.handshake.headers } as express.Request;
+        const res = {} as express.Response;
+
+        void cookiePraserWithEncryptionMiddleware(req, res, (err?: unknown) => {
+            if (err !== undefined) {
+                if (err instanceof Error) {
+                    return void next(err);
+                }
+                else {
+                    return void next(new Error("Somthing went wrong with socket cookie praser"));
+                };
+            }
+            socket.data.cookies = req.cookies || undefined;
+            socket.data.signedCookies = req.signedCookies || undefined;
+            next();
+        });
+    }
+    catch (err) {
+        appLogger.error(err);
+        if (err instanceof Error) {
+            return void next(err);
+        }
+        else {
+            return void next(new Error("Somthing went wrong with socket cookie praser"));
+        };
+    }
+};
