283446abf8da952a89651f12f2c68b201f58d2d7
Rohan Paul <75577230+NotRohanPaul@users.noreply.github.com>
2025-08-23 14:52:27 +0530

    backend: socket-middleware - added auth middleware to supply the email to the socket data for use in future event listener logic

---
+++ backend/src/sockets/middlewares/socket-middlewares/auth-socket.middleware.ts
@@ -0,0 +1,52 @@
+import { JWT_SECRET } from "@src/constants/env";
+import jwt from "jsonwebtoken";
+import type { SocketMiddlewareType } from "../../types";
+import { appLogger } from "@src/configs/app-logger";
+import { cookiesSchema, jwtPayloadLooseTransformSchema } from "@src/schemas/auth-schemas";
+
+export const socketAuthMiddleware: SocketMiddlewareType<{
+    context: {
+        auth: Record<string, unknown>;
+    },
+    socketData: {
+        signedCookies?: {
+            accessToken?: string;
+        };
+    };
+}> = (socket, next) => {
+    try {
+        const cookies = socket.data.signedCookies;
+        if (cookies === undefined) {
+            appLogger.error("No Cookies");
+            return void next(new Error("Unautorized Access"));
+        }
+
+        const prasedCookieAccessToken = cookiesSchema.shape.accessToken.safeParse(cookies.accessToken);
+        if (prasedCookieAccessToken.success === false) {
+            appLogger.error(prasedCookieAccessToken.error, "Cookie Prasing Error");
+            return void next(new Error("Unautorized Access"));
+        }
+
+        const jwtPayload = jwt.verify(prasedCookieAccessToken.data, JWT_SECRET);
+        const payloadParsedResult = jwtPayloadLooseTransformSchema.safeParse(jwtPayload);
+        if (payloadParsedResult.success === false) {
+            appLogger.error(payloadParsedResult.error, "JWT Payload Parsing Error");
+            return void next(new Error("Unautorized Access"));
+        }
+
+        socket.data.context ||= {};
+        socket.data.context.auth = {
+            email: payloadParsedResult.data.email
+        };
+        next();
+    }
+    catch (err) {
+        appLogger.error(err);
+        if (err instanceof Error) {
+            return void next(err);
+        }
+        else {
+            return void next(new Error("Somthing went wrong with socket auth middleware"));
+        };
+    }
+};
