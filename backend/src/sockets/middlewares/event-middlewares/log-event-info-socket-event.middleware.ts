a8e796cdebcf8feb66085083e1c9ef072621e432
Rohan Paul <75577230+NotRohanPaul@users.noreply.github.com>
2025-08-23 14:50:57 +0530

    backend: socket-event-middleware - added logger to get events and args

---
+++ backend/src/sockets/middlewares/event-middlewares/log-event-info-socket-event.middleware.ts
@@ -0,0 +1,7 @@
+import { appLogger } from "@src/configs/app-logger";
+import type { EventMiddlewareType } from "../../types";
+
+export const logEventInfoSocketEventMiddleware: EventMiddlewareType = (event, next) => {
+    appLogger.info({ args: event.slice(1) }, `Event received: ${event[0]}`);
+    next();
+};
