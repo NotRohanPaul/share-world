4a071d64434b252a3146f28a4f21f3a4b57e258c
Rohan Paul <75577230+NotRohanPaul@users.noreply.github.com>
2025-08-27 13:55:08 +0530

    frontend: provider appearance - added light and dark functionality using a app provider

---
+++ frontend/src/providers/ui/app-appearance-provider.tsx
@@ -0,0 +1,29 @@
+import { useAppSelector } from "@src/redux/utils/hooks";
+import { settingsSelectors } from "@src/redux/slices/settings";
+import { type PropsWithChildren, useEffect } from "react";
+import { MotionGlobalConfig } from "motion/react";
+
+
+export const AppAppearanceProvider = ({ children }: PropsWithChildren) => {
+    const { theme, disableAnimations } = useAppSelector(settingsSelectors.appearance);
+
+    useEffect(() => {
+        const root = document.documentElement;
+        if (theme === "dark") {
+            root.classList.add("dark");
+        } else {
+            root.classList.remove("dark");
+        }
+    }, [theme]);
+
+
+    useEffect(() => {
+        MotionGlobalConfig.skipAnimations = disableAnimations;
+    }, [disableAnimations]);
+
+    return (
+        <>
+            {children}
+        </>
+    );
+};
--- frontend/src/providers/ui/index.tsx
+++ frontend/src/providers/ui/index.tsx
@@ -1,13 +1,16 @@
 import { DialogBoxProvider } from "@src/components/common/ui/dialog-box/context/dialog-box-provider";
 import { ToastsProvider } from "@src/components/common/ui/toast/context/toasts-provider";
 import type { PropsWithChildren } from "react";
+import { AppAppearanceProvider } from "./app-appearance-provider";
 
 
 
 export const UIProviders = ({ children }: PropsWithChildren) => (
     <DialogBoxProvider>
         <ToastsProvider>
-            {children}
+            <AppAppearanceProvider>
+                {children}
+            </AppAppearanceProvider>
         </ToastsProvider>
     </DialogBoxProvider >
 );
