08185c5db3070e03e4a6aea2155058547108e2dc
Rohan Paul <75577230+NotRohanPaul@users.noreply.github.com>
2025-08-29 19:23:33 +0530

    frontend: sockets hook - made the sender friend socket hook listen to events to get online friends list and also use refresh endpoint to get new access token if the accesstoken expired

--- frontend/src/components/share-to-friend/sender/hooks/useSenderFriendSocket.ts
+++ frontend/src/components/share-to-friend/sender/hooks/useSenderFriendSocket.ts
@@ -1,6 +1,9 @@
+import { apiHandlers } from "@src/axios/handlers/api-handlers";
+import { useToastConsumer } from "@src/components/common/ui/toast/context/toasts-consumer";
+import { HTTP_STATUS_CODES } from "@src/constants/http-status-codes";
 import { shareActions } from "@src/redux/slices/share";
 import { useAppDispatch } from "@src/redux/utils/hooks";
-import { shareToFriendSocketInstance } from "@src/sockets/socket-instance";
+import { shareToFriendSocketInstance } from "@src/sockets/socket-instances";
 import { useEffect, useRef } from "react";
 import type { Socket } from "socket.io-client";
 
@@ -8,46 +11,57 @@ import type { Socket } from "socket.io-client";
 export const useSenderFriendSocket = () => {
     const socketRef = useRef<Socket | null>(null);
     const dispatch = useAppDispatch();
+    const showToast = useToastConsumer();
 
     useEffect(() => {
-        const io = shareToFriendSocketInstance.connect();
-        socketRef.current = io;
+        const socket = shareToFriendSocketInstance.connect();
+        socketRef.current = socket;
 
-        io.on("connect", () => {
-            console.log("coonected");
+        socket.on("connect", () => {
+            console.log("connected");
+            socket.emit("online-friends-list-server");
         });
 
-        io.on("online-friends-list-client", ({
-            onlineFriendsEmailList
+        socket.on("connect_error", async (err) => {
+            console.log("Connection error:", { err });
+            if (err.message === "Unautorized Access") {
+                try {
+                    console.log("Refreshing token");
+                    const refreshResponse = await apiHandlers.auth.refresh();
+                    if (refreshResponse.status === HTTP_STATUS_CODES.UNAUTHORIZED) {
+                        showToast({ text: "Session expired. Please login again" });
+                    }
+                } catch {
+                    showToast({ text: "Network Error" });
+                }
+            }
+        });
+
+
+        socket.on("online-friends-list-client", ({
+            onlineFriendsList
         }: {
-            onlineFriendsEmailList: string[];
+            onlineFriendsList: Record<string, string>[];
         }) => {
+            console.log(onlineFriendsList);
             dispatch(shareActions
                 .shareToFriend
-                .setOnlineFriendsEmailList(
-                    onlineFriendsEmailList
+                .setOnlineFriendsList(
+                    onlineFriendsList
                 ));
         });
 
-        io.on("receiver-friend-conformation-client", ({
-            onlineFriendsEmailList
-        }: {
-            onlineFriendsEmailList: string[];
-        }) => {
-            dispatch(shareActions
-                .shareToFriend
-                .setOnlineFriendsEmailList(
-                    onlineFriendsEmailList
-                ));
-        });
+        /* socket.on("receiver-friend-conformation-client", () => {
+           
+        }); */
 
         return () => {
-            io.disconnect();
+            socket.disconnect();
         };
 
     }, []);
 
     return {
-
+        socketRef
     };
 };
