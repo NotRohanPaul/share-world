cf3b21df875cde444632faa76a12f265374cc20f
Rohan Paul <75577230+NotRohanPaul@users.noreply.github.com>
2025-08-29 18:00:13 +0530

    backend: share to friend namespace - added types and basic events to shareToFriend namespace

--- backend/src/sockets/namespaces/share-to-friend/share-to-friend-socket-namespace.ts
+++ backend/src/sockets/namespaces/share-to-friend/share-to-friend-socket-namespace.ts
@@ -1,19 +1,58 @@
+import { UsersModel } from "@src/models/core-models";
 import { logEventInfoSocketEventMiddleware } from "@src/sockets/middlewares/event-middlewares/log-event-info-socket-event.middleware";
 import { socketAuthMiddleware } from "@src/sockets/middlewares/socket-middlewares/auth-socket.middleware";
 import { socketCookieMiddleware } from "@src/sockets/middlewares/socket-middlewares/cookie-parser-socket.middleware";
-import type { SocketType } from "@src/sockets/types/socket-types";
 import type { Server } from "socket.io";
+import type { ShareToFriendNamespaceType } from "./types/share-to-friend-types";
 
 
 export const shareToFriendNamespace = (io: Server): void => {
     const ns = io
         .of("/share-to-friend")
         .use(socketCookieMiddleware)
-        .use(socketAuthMiddleware);
+        .use(socketAuthMiddleware) as ShareToFriendNamespaceType;
 
-    ns.on("connection", (socket: SocketType) => {
+    const onlineUsers = new Map<string, string>();
+    ns.on("connection", (socket) => {
         socket.use(logEventInfoSocketEventMiddleware);
+        const userEmail = socket.data.context?.auth?.email;
+        if (userEmail !== undefined) {
+            onlineUsers.set(userEmail, socket.id);
+        }
 
+        socket.on("disconnect", () => {
+            if (userEmail !== undefined) {
+                onlineUsers.delete(userEmail);
+            }
+        });
 
+        socket.on("online-friends-list-server", async () => {
+            const userDoc = await UsersModel.findOne(
+                { email: userEmail },
+                { friendsEmailList: 1 }
+            );
+            
+            if (userDoc === null) {
+                return void socket.emit("online-friends-list-client", { onlineFriendsList: [] });
+            }
+
+            const onlineFriendsEmails = userDoc.friendsEmailList.filter(
+                (friendEmail: string) => onlineUsers.has(friendEmail)
+            );
+
+            const onlineFriends = await UsersModel.find(
+                { email: { $in: onlineFriendsEmails } },
+                { email: 1, name: 1 }
+            );
+            console.log(onlineFriends);
+            const results = onlineFriends.map((friend) => {
+                return {
+                    name: friend.name,
+                    email: friend.email,
+                };
+            });
+
+            socket.emit("online-friends-list-client", { onlineFriendsList: results });
+        });
     });
 };
---
+++ backend/src/sockets/namespaces/share-to-friend/types/share-to-friend-types.ts
@@ -0,0 +1,79 @@
+import type { SocketNamespaceType, SocketType } from "@src/sockets/types/socket-generic-types";
+
+type FriendPairEventsType = {
+    listenEvents: {
+        "online-friends-list-server": [],
+        "receiver-friend-info-server": [{ receiver: Record<string, string>, }],
+        "receiver-friend-accept-request-server": [{ sender: string, }],
+    },
+    emitEvents: {
+        "online-friends-list-client": [{ onlineFriendsList: { name: string, email: string; }[], }],
+        "receiver-friend-connection-request-client": [{ sender: Record<string, string>, }],
+    },
+};
+
+type WebRTCEventsType = {
+    listenEvents: {
+        "webrtc-offer-server": [{
+            to: string,
+            offer: Record<string, unknown>,
+        }],
+
+        "webrtc-answer-server": [{
+            to: string,
+            answer: Record<string, unknown>,
+        }],
+
+        "webrtc-ice-candidate-server": [{
+            to: string,
+            candidate: Record<string, unknown>,
+        }],
+    },
+
+    emitEvents: {
+        "webrtc-offer-client": [{
+            offer: Record<string, unknown>,
+        }],
+
+        "webrtc-answer-client": [{
+            answer: Record<string, unknown>,
+        }],
+
+        "webrtc-ice-candidate-client": [{
+            candidate: Record<string, unknown>,
+        }],
+    },
+};
+
+type ShareEventsType = {
+    listenEvents: FriendPairEventsType["listenEvents"] & WebRTCEventsType["listenEvents"],
+    emitEvents: FriendPairEventsType["emitEvents"] & WebRTCEventsType["emitEvents"] & {
+        "error": [{ message: string, }],
+    },
+    context: { auth: { email: string; }, },
+};
+
+
+export type ShareToFriendNamespaceType = SocketNamespaceType<{
+    listenEvents: ShareEventsType["listenEvents"],
+    emitEvents: ShareEventsType["emitEvents"],
+    context: ShareEventsType["context"],
+}>;
+
+export type ShareToFriendSocketType = SocketType<{
+    listenEvents: ShareEventsType["listenEvents"],
+    emitEvents: ShareEventsType["emitEvents"],
+    context: ShareEventsType["context"],
+}>;
+
+export type FriendPairSocketType = SocketType<{
+    listenEvents: FriendPairEventsType["listenEvents"],
+    emitEvents: FriendPairEventsType["emitEvents"],
+    context: ShareEventsType["context"],
+}>;
+
+export type WebRTCSocketType = SocketType<{
+    listenEvents: WebRTCEventsType["listenEvents"],
+    emitEvents: WebRTCEventsType["emitEvents"],
+    context: ShareEventsType["context"],
+}>;
