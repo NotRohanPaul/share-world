46f24c2edcce5447dc03bc0083eb689ee45e26d4
Rohan Paul <75577230+NotRohanPaul@users.noreply.github.com>
2025-09-10 14:23:24 +0530

    backend: gracefull shutdown - made the gracefull shutdown modular and added events handlers for mogodb and redis connenction failures

--- backend/src/configs/graceful-shutdown.ts
+++ backend/src/configs/graceful-shutdown.ts
@@ -1,44 +1,95 @@
 import mongoose from "mongoose";
 import type { Server } from "node:http";
 import { appLogger } from "./app-logger";
+import { pubClient, redis, subClient } from "./redis-client";
+
+const shutdownMogodb = async (): Promise<void> => {
+    appLogger.info("Closing MongoDB connection");
+    await mongoose.connection.close(false);
+    appLogger.info("MongoDB connection closed");
+};
+
+const shutdownRedis = async (): Promise<void> => {
+    try {
+        appLogger.info("Closing Redis clients...");
+
+        await redis.quit();
+        appLogger.info("General Redis client closed");
+
+        await pubClient.quit();
+        appLogger.info("Pub Redis client closed");
+
+        await subClient.quit();
+        appLogger.info("Sub Redis client closed");
+
+        redis.disconnect();
+        pubClient.disconnect();
+        subClient.disconnect();
+    } catch (err) {
+        appLogger.error(err, "Error closing Redis clients");
+    }
+};
+
+//To prevent multiple calls for termination
+let isShuttingDown = false;
+const shutdownServer = (server: Server): void => {
+    if (isShuttingDown === true) return;
+    isShuttingDown = true;
+
+    appLogger.info("Shutting down server...");
+
+    server.on("close", () => {
+        appLogger.info("Server has closed");
+    });
+    server.close(() => {
+        void (async (): Promise<void> => {
+            try {
+                await shutdownRedis();
+                await shutdownMogodb();
+                process.exit(0);
+            }
+            catch (err) {
+                appLogger.error(err, "Something went wrong during server closing \n");
+                process.exit(1);
+            }
+        })();
+    });
+};
 
 export const gracefulShutdown = (server: Server): void => {
-    //To prevent multiple calls for termination
-    let isShuttingDown = false;
-
-    function shutdown(): void {
-        if (isShuttingDown === true) return;
-        isShuttingDown = true;
-
-        appLogger.info("Shutting down server...");
-
-        server.on("close", () => {
-            appLogger.info("Server has closed");
+    [
+        { client: redis, name: "General" },
+        { client: pubClient, name: "Publisher" },
+        { client: subClient, name: "Subscriber" }
+    ].forEach(({ client, name }) => {
+        client.on("end", () => {
+            appLogger.error(`${name} Redis client connection closed permanently.`);
+            shutdownServer(server);
         });
-        server.close(() => {
-            void (async (): Promise<void> => {
-                try {
-                    appLogger.info("Closing MongoDB connection");
-                    await mongoose.connection.close(false);
-                    appLogger.info("MongoDB connection closed");
-                    process.exit(0);
-                }
-                catch (err) {
-                    appLogger.error(err, "Something went wrong during MongoDB connection closing \n");
-                    process.exit(1);
-                }
-            })();
+
+        client.on("error", (err) => {
+            appLogger.error(err, `${name} Redis client error`);
         });
-    }
+    });
+
+    mongoose.connection.on("disconnected", () => {
+        appLogger.error("MongoDB disconnected.");
+        shutdownServer(server);
+    });
+
+    mongoose.connection.on("error", (err) => {
+        appLogger.error(err, "MongoDB connection error.");
+        shutdownServer(server);
+    });
 
     process.on("unhandledRejection", (reason, origin) => {
         appLogger.error({ reason, origin }, "Unhandled Rejection at: ",);
-        shutdown();
+        shutdownServer(server);
     });
     process.on("uncaughtException", (error, origin) => {
         appLogger.error({ error, origin }, "Uncaught Exception at: ");
-        shutdown();
+        shutdownServer(server);
     });
-    process.on("SIGINT", shutdown);
-    process.on("SIGTERM", shutdown);
+    process.on("SIGINT", () => shutdownServer(server));
+    process.on("SIGTERM", () => shutdownServer(server));
 };
